02bf55f7d03afcf0db9eca9300136dd9327f2fc2
Olu-martins <mayowasamuel@gmail.com>
2024-07-05 12:41:32 +0100

    update and format price

---
+++ pages/utils/index.tsx
@@ -0,0 +1,5 @@
+export const formatAmount = (value: unknown) => {
+    const unformattedValue = value?.toString().replace(/,/g, '');
+    const formattedValue = unformattedValue ? parseFloat(unformattedValue).toFixed(2) : '';
+    return `${formattedValue}`.replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,');
+};
